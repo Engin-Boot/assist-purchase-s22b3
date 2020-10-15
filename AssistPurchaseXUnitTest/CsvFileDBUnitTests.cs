b4b718dfa22526d6f9fa70bfc9305bb206446db5
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-10-15 17:31:00 +0530

    Update and rename FileDBUnitTests.cs to CsvFileDBUnitTests.cs

--- AssistPurchaseXUnitTest/CsvFileDBUnitTests.cs
+++ AssistPurchaseXUnitTest/CsvFileDBUnitTests.cs
@@ -1,4 +1,4 @@
-﻿using AssistPurchase.Models;
+using AssistPurchase.Models;
 using AssistPurchase.Utility;
 using System;
 using System.Collections.Generic;
@@ -8,33 +8,35 @@ using Xunit;
 
 namespace AssistPurchaseXUnitTest
 {
-    public class FileDBUnitTests
+    public class CsvFileDBUnitTests
     {
         private string _csvFilePath =  Directory.GetCurrentDirectory()+@"\TestingSource\patientMonitorTest.csv";
         IFileHandler csvHandler = new CsvFileHandler();
 
         [Fact]
-        public void WhenReadFileMethodIsCalledReturnsMoniterProductData()
+        public void WhenValidFilePathIsGivenReturnsListOfMoniterProductData()
         {
             var patientMonitors = this.csvHandler.ReadFile(_csvFilePath);
             Assert.NotEmpty(patientMonitors);
         }
 
         [Fact]
-        public void WhenReadFileMethodIsCalledReturnEmptyListForNotSpecifyingCorrectFilePath()
+        public void WhenIncorrectFilePathIsGivenReturnsEmptyList()
         {
-            _csvFilePath = Directory.GetCurrentDirectory() + @"\patientMonitorTest1.csv";
+            _csvFilePath = Directory.GetCurrentDirectory() + @"patientMonitorTest1.csv";
             var patientMonitors = this.csvHandler.ReadFile(_csvFilePath);
             Assert.Empty(patientMonitors);
         }
 
         [Fact]
-        public void WhenReadFileMethodIsCalledReturnEmptyListIfDataFormatIsIncorrect()
+        public void WhenIncorrectDataFileFormatIsGivenReturnsEmptyList()
         {
             _csvFilePath = Directory.GetCurrentDirectory() + @"\TestingSource\patientMonitorIncorrectDataTest.csv";
             var patientMonitors = this.csvHandler.ReadFile(_csvFilePath);
             Assert.Empty(patientMonitors);
         }
+
+        
 
         [Fact]
         public void WhenWriteToFileMethodIsCalledMonitorProductDataIsAppendedToFile()
@@ -55,6 +57,25 @@ namespace AssistPurchaseXUnitTest
         }
 
         [Fact]
+        public void WhenNonExistingFileNameIsGivenReturnsFalseValue()
+        {
+            _csvFilePath = Directory.GetCurrentDirectory() + @"\patientMonitorTest1.csv";
+            PatientMonitor patientMonitor = new PatientMonitor();
+            patientMonitor.MonitorId = "P6";
+            patientMonitor.MonitorName = "XYZ";
+            patientMonitor.MonitorDescription = "bfjvbdc bnv";
+            patientMonitor.MonitorPhysicalSpecification = new PhysicalSpecificationDataModel(15.3, new ProductSizeDataModel(15, 17, 11));
+            patientMonitor.MonitorMeasurementsSpecification = new MeasurementSpecificationDataModel(new List<string> { "ECG", " SPO2", "Respiration" });
+            patientMonitor.MonitorDisplaySpecification = new DisplaySpecificationDataModel(40, "1024X348");
+            patientMonitor.MonitorBatterySpecification = new BatterySpecificationDataModel(10);
+            patientMonitor.ProductImage = new ProductImageDataModel(Directory.GetCurrentDirectory() + @"\TestingSource\TestImage.jpg");
+
+
+            bool isAdded = this.csvHandler.WriteToFile(patientMonitor, _csvFilePath);
+            Assert.False(isAdded);
+        }
+
+        [Fact]
         public void WhenNullDataIsWrittenInFileThenReturnsFalseValue()
         {
             PatientMonitor patientMonitor = new PatientMonitor();
@@ -64,7 +85,7 @@ namespace AssistPurchaseXUnitTest
         }
 
         [Fact]
-        public void WhenDeleteFromFileMethodCalledProductIsDeletedBasedOnIdSuccess()
+        public void WhenExistingProductIdIsPassedThenDeletsProductDetailsAndReturnsTrue()
         {
             string id = "P5";
 
@@ -73,11 +94,20 @@ namespace AssistPurchaseXUnitTest
         }
 
         [Fact]
-        public void WhenDeleteFromFileMethodCalledProductIsDeletedBasedOnIdFail()
+        public void WhenNonExistingProductIdIsPassedThenReturnsFalse()
         {
             string id = "P11";
 
             var isDeleted = this.csvHandler.DeleteFromFile(id, _csvFilePath);
+            Assert.False(isDeleted);
+        }
+
+        [Fact]
+        public void WhenFileNameIsPassedAsNullReturnsFalseValue()
+        {
+            string id = "P5";
+
+            var isDeleted = this.csvHandler.DeleteFromFile(id, null);
             Assert.False(isDeleted);
         }
     }
