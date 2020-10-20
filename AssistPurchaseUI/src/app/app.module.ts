f4fc96d282fac642fc2932aec810811b98352009
Varsha <45787710+Varsha-Chalasani@users.noreply.github.com>
2020-10-20 13:33:19 +0530

    Pushing Components

--- AssistPurchaseUI/src/app/app.module.ts
+++ AssistPurchaseUI/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component'
+import {BrowserModule} from '@angular/platform-browser'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+    declarations:[HomeComponent],
+    bootstrap:[HomeComponent],
+    imports:[BrowserModule],
+    /*exports:[GreeterComponent],
+    providers:[
+      {provide:'logger',useClass:ApiLoggerService},
+      {provide:'apiBaseAddress',useValue:"http://localhost:8003"}
+    ]*/
+  })
+
+export class AppModule{
+
+}
---
+++ AssistPurchaseUI/src/app/customer-form/customer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'customerform-comp',
+  templateUrl: './customer-form.component.html',
+  styleUrls: ['./customer-form.component.css']
+})
+export class CustomerFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
